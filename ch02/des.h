b01e6e40cf12e944a602d805cdb57e403cfb5ab7
cuongth <bronzeboyvn@gmail.com>
2014-11-24 21:23:19 +0700

    DES Electronic Code Book

---
+++ ch02/des.h
@@ -0,0 +1,19 @@
+#ifndef DES_H
+#define DES_H
+
+#define DES_BLOCK_SIZE 8 // 64 bits, defined in the standard
+#define DES_KEY_SIZE 8 // 56 bits used, but must supply 64 (8 are ignored)
+#define EXPANSION_BLOCK_SIZE 6
+#define PC1_KEY_SIZE 7
+#define SUBKEY_SIZE 6
+
+void des_encrypt( const unsigned char *plaintext,
+    const int plaintext_len, unsigned char *ciphertext,
+    const unsigned char *key );
+
+
+void des_decrypt( const unsigned char *input,
+    int input_len, unsigned char *output,
+    const unsigned char *key );
+
+#endif
